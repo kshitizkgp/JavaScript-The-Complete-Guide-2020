e40f7af6a0045d2be787a5681d935a63c3843395
kshkumar <kshkumar@adobe.com>
2020-06-04 17:29:11 +0530

    Adding adding to cart logic and update amount on add.

--- oop-03-productlist-productitem-classes/assets/scripts/app.js
+++ oop-03-productlist-productitem-classes/assets/scripts/app.js
@@ -1,26 +1,58 @@
-class Product {
-  // title = 'DEFAULT';
-  // imageUrl;
-  // description;
-  // price;
+class Component {
 
-  constructor(title, image, desc, price) {
-    this.title = title;
-    this.imageUrl = image;
-    this.description = desc;
-    this.price = price;
-  }
+    constructor(renderHookId, shouldRender = true) {
+        this.hookId = renderHookId;
+        if(shouldRender)
+            this.render();
+    }
+
+    render() {
+    }
+
+    createRootElement(tag, cssClasses, attributes) {
+        const rootElement = document.createElement(tag);
+        if (cssClasses) {
+            rootElement.className = cssClasses;
+        }
+        if (attributes && attributes.length > 0) {
+            for (const attr of attributes) {
+                rootElement.setAttribute(attr.name, attr.value);
+            }
+        }
+        document.getElementById(this.hookId).append(rootElement);
+        return rootElement;
+    }
 }
 
-class ProductItem {
-  constructor(product) {
-    this.product = product;
-  }
+class Product {
+    // title = 'DEFAULT';
+    // imageUrl;
+    // description;
+    // price;
 
-  render() {
-    const prodEl = document.createElement('li');
-    prodEl.className = 'product-item';
-    prodEl.innerHTML = `
+    constructor(title, image, desc, price) {
+        this.title = title;
+        this.imageUrl = image;
+        this.description = desc;
+        this.price = price;
+    }
+}
+
+class ProductItem extends Component {
+
+    constructor(product, hookId) {
+        super(hookId, false);
+        this.product = product;
+        this.render();
+    }
+
+    addToCart() {
+        App.addProductToCart(this.product);
+    }
+
+    render() {
+        const prodEl = this.createRootElement('li', 'product-item');
+        prodEl.innerHTML = `
         <div>
           <img src="${this.product.imageUrl}" alt="${this.product.title}" >
           <div class="product-item__content">
@@ -31,40 +63,128 @@ class ProductItem {
           </div>
         </div>
       `;
-    return prodEl;
-  }
-}
-
-class ProductList {
-  products = [
-    new Product(
-      'A Pillow',
-      'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
-      'A soft pillow!',
-      19.99
-    ),
-    new Product(
-      'A Carpet',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
-      'A carpet which you might like - or not.',
-      89.99
-    )
-  ];
-
-  constructor() {}
-
-  render() {
-    const renderHook = document.getElementById('app');
-    const prodList = document.createElement('ul');
-    prodList.className = 'product-list';
-    for (const prod of this.products) {
-      const productItem = new ProductItem(prod);
-      const prodEl = productItem.render();
-      prodList.append(prodEl);
+        const addCartButton = prodEl.querySelector('button');
+        addCartButton.addEventListener('click', this.addToCart.bind(this));
     }
-    renderHook.append(prodList);
-  }
 }
 
-const productList = new ProductList();
-productList.render();
+class ElementAttribute {
+    constructor(name, value) {
+        this.name = name;
+        this.value = value;
+    }
+}
+
+class ProductList extends Component {
+    products = [];
+
+    constructor(hookId) {
+        super(hookId);
+        this.fetchProducts();
+    }
+
+    fetchProducts(){
+        this.products = [
+            new Product(
+                'A Pillow',
+                'https://thumbs.dreamstime.com/b/bed-pillow-white-fluffy-isolated-white-background-84020025.jpg',
+                'A soft pillow!',
+                19.99
+            ),
+            new Product(
+                'A Carpet',
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
+                'A carpet which you might like - or not.',
+                89.99
+            )
+        ];
+        this.renderProducts();
+    }
+
+    renderProducts(){
+        for (const prod of this.products) {
+            new ProductItem(prod, 'prod-list');
+        }
+    }
+
+    render() {
+        const prodList = this.createRootElement('ul', 'product-list', [
+            new ElementAttribute('id', 'prod-list')
+        ]);
+
+        if(this.products && this.products.length > 0){
+            this.renderProducts();
+        }
+    }
+}
+
+class ShoppingCart extends Component {
+    items = [];
+
+    constructor(hookId) {
+        super(hookId);
+    }
+
+    set cartItems(value) {
+        this.items = value;
+        this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(2)}</h2>`
+    }
+
+    get totalAmount() {
+        const sum = this.items.reduce((prevValue, curItem) => prevValue + curItem.price,
+                                      0
+        );
+        return sum;
+    }
+
+    addProduct(product) {
+        const updatedItems = [...this.items];
+        updatedItems.push(product);
+        this.cartItems = updatedItems;
+    }
+
+    orderProducts() {
+        console.log("Ordering products...");
+        console.log(this.items);
+    }
+
+    render() {
+        const cartEl = this.createRootElement('section', 'cart')
+        cartEl.innerHTML = `
+            <h2>Total: \$${0}</h2>
+            <button>Order Now!</button>
+        `;
+        this.totalOutput = cartEl.querySelector('h2');
+        const orderButton = document.querySelector('button');
+        orderButton.addEventListener('click', this.orderProducts.bind(this));
+    }
+}
+
+class Shop extends Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        this.cart = new ShoppingCart('app');
+        new ProductList('app');
+    }
+}
+
+class App {
+    static cart;
+
+    static init() {
+        const shop = new Shop();
+        this.cart = shop.cart;
+    }
+
+    static addProductToCart(product) {
+        this.cart.addProduct(product);
+    }
+}
+
+App.init();
+
+
